e2739945f006f250d5b1d21d72fedc895b6921c6
Renato <renato.desarrollador@gmail.com>
2024-05-26 14:01:09 -0500

    circuit-breaker

--- config-service/src/main/java/com/config/service/ConfigServiceApplication.java
+++ config-service/src/main/java/com/config/service/ConfigServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer//Habilita el servidor de configuración.
+@EnableConfigServer
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
